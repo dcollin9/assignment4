6b626b07399e0eba4cf845ad256d9bc5c5f16b25
dcollin9 <70920220+dcollin9@users.noreply.github.com>
2021-02-05 12:12:20 -0700

    Just finished the first model

---
+++ Assignment4/Models/FavRestaurants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment4.Models
+{
+    public class FavRestaurants
+    {
+        [Required]
+        public int Ranks { get; set; }
+
+        [Required]
+        public string RestaurantName { get; set; } = "It's all tasty!";
+
+        
+        public string FavDish { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        public string Website { get; set; }
+
+    }
+}
